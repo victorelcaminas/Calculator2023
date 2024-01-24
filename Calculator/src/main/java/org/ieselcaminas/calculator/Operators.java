eaf76da9e4caff36124412ed9fe43c8f00d2fc44
vmalonsobarberan <vm.alonsobarberan@edu.gva.es>
2024-01-24 17:39:02 +0100

    Operators

---
+++ Calculator/src/main/java/org/ieselcaminas/calculator/Operators.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package org.ieselcaminas.calculator;
+
+/**
+ *
+ * @author puntual
+ */
+public enum Operators {
+    NONE, ADD, MULT, DIV, SUB
+    
+}
